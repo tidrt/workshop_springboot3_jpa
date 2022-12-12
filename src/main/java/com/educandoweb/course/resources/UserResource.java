188feaeba1d5dab99e1da6e5fe5006a4f89890e1
tiago <tiago@ufrgs.br>
2022-12-12 00:13:38 -0300

    service layer and component registration

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,11 +1,16 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -13,11 +18,23 @@ public class UserResource {
 	/* basic resource based on the User class,
 	a web resource implemented by a rest controller */
 	
+	@Autowired
+	private UserService service;
+	
 	// end point to access the users
 	// response entities a specific type to return web requests answers
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll(); 
+		return ResponseEntity.ok().body(list);
 	}
+	
+	// end point to access user by id
+	// to Spring accept the parameter we need to use PathVariable annotation
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id); 
+		return ResponseEntity.ok().body(obj);  
+	}
+	
 }
