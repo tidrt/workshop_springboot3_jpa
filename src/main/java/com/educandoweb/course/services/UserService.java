5d7f7d4419d1e4f4c5897588add87b8e9d000c21
tiago <tiago@ufrgs.br>
2023-01-08 12:44:52 -0300

    exception handling - update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -44,9 +46,13 @@ public class UserService {
 	
 	public User update(Long id, User obj) {
 		// an entity monitored by JPA
-		User entity = repository.getReferenceById(id); // this get doesn't go directly to the database
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id); // this get doesn't go directly to the database
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	// update the data on entity using as a base the data that we have in the obj
